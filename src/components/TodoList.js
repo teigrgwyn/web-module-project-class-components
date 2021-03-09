c94a44b2e306c7116ae78d8338e465c49cfd3f75
Cory Young <teigrgwyn@gmail.com>
2021-03-09 11:36:45 -0600

    MVP

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,13 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = props => {
+  return (
+    // returns an identical key warning on first render likely due to the default state set
+    props.todoList.map(todo => (
+      <Todo key={todo.id} task={todo} todo={todo} onClickTask={props.onClickTask} />
+    ))
+  )
+}
+
+export default TodoList;
